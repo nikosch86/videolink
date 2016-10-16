ad552dc7335726d088af35aad1511571a1b30f93
Nikolaus Schuster <nikolaus@tlt.local>
2016-10-16 08:14:38 +0400

    first stab at multi room

--- server/peerserver.js
+++ server/peerserver.js
@@ -4,6 +4,7 @@ var PeerServer = require('peer').PeerServer;
 var fs = require('fs');
 var server = PeerServer({
   path: '/',
+  port: 8080,
   ssl: {
     key: fs.readFileSync('/etc/lighttpd/ssl/server.key'),
     cert: fs.readFileSync('/etc/lighttpd/ssl/server.crt')
